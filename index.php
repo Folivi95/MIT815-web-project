324bfcc0e7908b4be0e2c8884db6f695144a7728
jfolivi <joshua.folivi@gmail.com>
2021-04-19 08:12:52 +0100

    added login page

--- index.php
+++ index.php
@@ -109,19 +109,17 @@ include('includes/navbar.php');
 
   <!-- Content Row -->
 
-<?php
-include('security.php');
-?>
+
 <div class="col-xl-3 col-md-6 mb-4">
     <div class="card border-left-primary shadow h-100 py-2">
         <div class="card-body">
             <div class="row no-gutters align-items-center">
                 <div class="col mr-2">
-                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Registered Admin</div>
+                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Classes</div>
                     <div class="h5 mb-0 font-weight-bold text-gray-800">
                         <?php
                 
-                            $query = "SELECT id FROM register ORDER BY id";  
+                            $query = "SELECT count(*) FROM classes ORDER BY id";  
                             $query_run = mysqli_query($connection, $query);
                             $row = mysqli_num_rows($query_run);
                             echo '<h4> Total Admin: '.$row.'</h4>';
